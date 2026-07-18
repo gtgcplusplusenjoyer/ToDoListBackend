3df060c2273e45694d9efb1623f6634de7f71d4a
AlexFedorets <sakhafedorets904@gmail.com>
2026-07-18 22:55:58 +0300

    feat: update generic repository interface

--- src/ToDoList.API/ToDoList.Core/Interfaces/Repositories/IRepository.cs
+++ src/ToDoList.API/ToDoList.Core/Interfaces/Repositories/IRepository.cs
@@ -7,8 +7,6 @@ namespace ToDoList.Core.Interfaces.Repositories
         Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
         Task<List<T>> GetAllAsync(CancellationToken cancellationToken);
         Task AddAsync(T entity, CancellationToken cancellationToken);
-        void Update(T entity);
-        void Delete(T entity);
         Task SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
--- src/ToDoList.API/ToDoList.Core/Interfaces/Repositories/IUserTaskRepository.cs
+++ src/ToDoList.API/ToDoList.Core/Interfaces/Repositories/IUserTaskRepository.cs
@@ -5,12 +5,13 @@ namespace ToDoList.Core.Interfaces.Repositories
 {
     public interface IUserTaskRepository : IRepository<UserTask>
     {
-        Task AddUserTaskAsync(UserTask userTask, CancellationToken cancellationToken);
         Task<UserTask?> GetUserTaskByIdAndUserIdAsync(Guid id, Guid userId, CancellationToken cancellationToken);
         Task<PagedResult<UserTask>> GetAllUsersTasksAsync(UserTaskFilter filter,
             SortParams sortParams,
             PageParams pageParams,
             Guid userId,
             CancellationToken cancellationToken);
+        void Update(UserTask userTask);
+        void Delete(UserTask userTask);
     }
 }
